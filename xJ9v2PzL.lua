eb126f9aea3f77c22efbc587dd789ef1062564c3
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2026-03-31 23:50:56 +0200

    Update xJ9v2PzL.lua

--- xJ9v2PzL.lua
+++ xJ9v2PzL.lua
@@ -15,13 +15,13 @@ local function isCurrentPlace(gameTable)
     return false
 end
 
-if placeId == PLACES.PETSIM99 then
+if isCurrentPlace(PLACES.PETSIM99) then
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/petsim99/refs/heads/main/%D0%B2%D1%81%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%BE%D0%B5/Gui/petsim.lua'))()
 
-elseif placeId == PLACES.EVADE then
+elseif isCurrentPlace(PLACES.EVADE) then
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/Evade/refs/heads/main/Evade.lua'))()
 
-elseif placeId == PLACES.BENPCORDIE then
+elseif isCurrentPlace(PLACES.BENPCORDIE) then
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Fishka132312/-FIRE-Be-NPC-or-DIE-/refs/heads/main/%5B%F0%9F%90%89FIRE%5D%20Be%20NPC%20or%20DIE!%F0%9F%92%A2.lua'))()
 
 else
